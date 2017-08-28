bb0b9d9a45cb226ea9ce1eb9084ec0406b603b37
LuqiangPeng <luqiangpeng@creditease.cn>
2017-08-28 10:25:20 +0800

    测试GMP

--- goroutine/testEchoSort.go
+++ goroutine/testEchoSort.go
@@ -1,21 +1,25 @@
 package main
 
 import (
+	"fmt"
+	"runtime"
 	"sync"
+	"time"
 )
 
 // GOMAXPROCS=1 go run echoSort.go
 // 结果: 9012345678
 func main() {
-	//runtime.GOMAXPROCS(1)
+	runtime.GOMAXPROCS(1)
 	wg := new(sync.WaitGroup)
 	for i := 0; i < 10; i++ {
 		wg.Add(1)
 		go func(j int) {
-			//time.Sleep(time.Duration(j) * time.Second)
-			for n := 0; n < 10000000000; n++ {
-
-			}
+			time.Sleep(time.Duration(1) * time.Second)
+			//for n := 0; n < 1000000000; n++ {
+			//
+			//}
+			fmt.Print(j)
 			wg.Done()
 		}(i)
 	}
---
+++ metrics/test.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+while true; do echo -n "example:$((RANDOM % 100))|c" | nc -w 1 -u 127.0.0.1 8125; done
